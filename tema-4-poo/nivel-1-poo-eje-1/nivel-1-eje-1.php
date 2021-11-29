4fe6b7c8658b6fb4ee78a8403f3a0c0805bc03d5
Inguema <inmis647@gmail.com>
2021-11-29 09:31:29 +0100

    Modificaciones en el código Nivel 1 > Tema 4

--- tema-4-poo/nivel-1-poo-eje-1/nivel-1-eje-1.php
+++ tema-4-poo/nivel-1-poo-eje-1/nivel-1-eje-1.php
@@ -1,30 +1,29 @@
 <?php
-/**Crear una clase Employee define como atributos su nombre y sueldo.
+/**
+ * Crear una clase Employee define como atributos su nombre y sueldo.
  * Definir un método initialize que reciba como parámetros el nombre y sueldo.
  * Plantear un segundo método print que imprima el nombre y un mensaje si debe
  * o no pagar impuestos (si el sueldo supera 6000 paga impuestos).
  */
+class Empleado {
 
-    class Empleado {
-        private $nombre;
-        private $sueldo;
+    private $nombre;
+    private $sueldo;
 
-        public function initialize ($nom, $sueldo) {
-            $this -> nombre = $nom;
-            $this -> sueldo = $sueldo;
-        }
-
-        public function imprimir () {
-
-            if ($this -> sueldo > 6000) {
-                echo $this -> nombre . "debe pagar impuestos";
-            } else {
-                echo $this -> nombre . " no debe pagar impuestos";
-            }
-
-        }
+    public function initialize(string $nombre, int $sueldo) {
+        $this->nombre = $nombre;
+        $this->sueldo = $sueldo;
     }
 
-    $obj = new Empleado;
-    $obj -> initialize("Andrea", 1500);
-    $obj -> imprimir();
+    public function imprimir() {
+        if ($this->sueldo > 6000) {
+            echo $this->nombre . " debe pagar impuestos.";
+        } else {
+            echo $this->nombre . " no debe pagar impuestos.";
+        }
+    }
+}
+
+$obj = new Empleado();
+$obj->initialize("Andrea", 6500);
+$obj->imprimir();
--- tema-4-poo/nivel-1-poo-eje-2/figura.php
+++ tema-4-poo/nivel-1-poo-eje-2/figura.php
@@ -1,26 +1,24 @@
 <?php
-/**Escribe un programa que defina una clase Shape con un constructor que reciba como parámetros el ancho y alto.
+/**
+ * Escribe un programa que defina una clase Shape con un constructor que reciba como parámetros el ancho y alto.
  * Define dos subclases; Triangle y Rectangle que hereden de Shape y que calculen respectivamente el área de la forma area().
  * En el archivo main define dos objetos, un triángulo y un rectángulo y llama al método area de cada uno.
  */
+class Figura {
 
-    class Figura {
+    //Atributos de la clase padre forma
+    public $ancho;
+    public $alto;
+    public $area;
 
-        //Atributos de la clase padre forma
-        public $ancho;
-        public $alto;
-        public $area;
-
-        //Constructor de clase
-        public function __construct ($base, $altura) {
-            $this -> ancho = $base;
-            $this -> alto = $altura;
-        }
-
-        //Métodos de clase
-        public function area () {
-
-            echo $this -> area . "<br/>";
-
-        }
+    //Constructor de clase
+    public function __construct ($base, $altura) {
+        $this->ancho = $base;
+        $this->alto = $altura;
     }
+
+    //Métodos de clase
+    public function area () {
+        echo $this->area . "<br/>";
+    }
+}
