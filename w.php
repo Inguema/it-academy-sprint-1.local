ad50da313b9e97b56303b8fce3f22a259c05e672
Inguema <inmis647@gmail.com>
2022-02-10 11:34:03 +0100

    función para depurar

---
+++ w.php
@@ -0,0 +1,14 @@
+<?php
+
+function w($data){
+    print_r('<pre>');
+    print_r($data);
+    print_r('</pre>');
+    die;
+}
+
+function wd($data){
+    print_r('<pre>');
+    print_r($data);
+    print_r('</pre>');
+}
